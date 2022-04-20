9c3b145b326bc06018d7019b32c71dcb99132f3c
DipeshRao10 <103821191+DipeshRao10@users.noreply.github.com>
2022-04-20 13:20:35 +0530

    branch 1
    Inserted data

--- GroupCStatus/src/babubhai/Salary.java
+++ GroupCStatus/src/babubhai/Salary.java
@@ -2,8 +2,12 @@ package babubhai;
 
 public class Salary {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+	{
+		System.out.println("Dipesh=====120000");
+		System.out.println("Suraj=====120000");
+		System.out.println("swati=====130000");
+		System.out.println("manjusha====140000");
 
 	}
 
